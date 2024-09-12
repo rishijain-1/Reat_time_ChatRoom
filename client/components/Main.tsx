c3616b9204e8bfc60622bdca2815cb3e58efc0f2
rishijain2002 <rishijain0652@gmail.com>
2024-09-13 01:09:44 +0530

    change port of server

--- client/components/Main.tsx
+++ client/components/Main.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import { CurrentUser } from './CurrentUser';
 
-const socket = io("http://localhost:3001");
+const socket = io("https://reat-time-chatroom.onrender.com");
 
 export const Main = () => {
     const [userName, setUserName] = useState(""); 
