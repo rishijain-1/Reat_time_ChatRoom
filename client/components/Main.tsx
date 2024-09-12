79394701ab7b786a0041bb2e4d1709240c6d5f33
rishijain2002 <rishijain0652@gmail.com>
2024-09-12 19:33:54 +0530

    add vercel server domain

--- client/components/Main.tsx
+++ client/components/Main.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io("http://localhost:3000");
+const socket = io("https://reat-time-chat-room.vercel.app");
 
 export const Main = () => {
     const [userName, setUserName] = useState(""); 
