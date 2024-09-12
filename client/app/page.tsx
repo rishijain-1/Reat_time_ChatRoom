505fe652501495bb2728926735a1cdc95e70edd7
rishijain2002 <rishijain0652@gmail.com>
2024-09-12 23:59:18 +0530

    import CurrentUser component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,17 @@
+import { CurrentUser } from "@/components/CurrentUser";
 import { Main } from "@/components/Main";
 
 export default function Home() {
   return (
-    <Main/>
+    <div className="flex h-screen">
+    <div className="flex-1 bg-white p-4">
+      <Main /> 
+    </div>
+
+    {/* Sidebar section for users */}
+    <div className="w-1/3 bg-gray-100 p-4">
+      <CurrentUser /> 
+    </div>
+  </div>
   );
 }
